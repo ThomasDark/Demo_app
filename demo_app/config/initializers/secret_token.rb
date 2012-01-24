6ab3b3859579519b3c5e985cae0e5a8b04ac3651
Thomas Dark <tom@vaultrms.com>
2012-01-24 09:51:33 -0700

    Initial Commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cadeb0932b76ad653cfa31550d92b373dfc4f77352d21b917d394329b75c6d8b392af3aab11a03a38515029e3e80aa46f01220e70d4cfeeba0df0db0ffc324c9'
